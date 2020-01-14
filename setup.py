f8e72724d165a5e98b1971b78776c9e198ca7da0
roland <roland@catalogix.se>
2020-01-14 21:14:40 +0100

    Fixed error in setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7"
     ],
     install_requires=["cryptography", "requests"],
