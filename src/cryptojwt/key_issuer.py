1d3ecc82029c9b65391e3f7ab1dd2ac08289d4a5
roland <roland@catalogix.se>
2020-05-19 11:43:24 +0200

    Repr similar to KeyJar.

--- src/cryptojwt/key_issuer.py
+++ src/cryptojwt/key_issuer.py
@@ -49,7 +49,7 @@ class KeyIssuer(object):
         self.httpc_params = httpc_params or {}
 
     def __repr__(self) -> str:
-        return '<KeyIssuer "{}"(KeyBundles={})>'.format(self.name, self._bundles)
+        return '<KeyIssuer "{}" {}>'.format(self.name, self.key_summary())
 
     def __getitem__(self, item):
         return self.get_bundles()[item]
