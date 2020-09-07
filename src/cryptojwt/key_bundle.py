a8506578490e8873d7a9602c4aacd6af9e0c2232
Jakob Schlyter <jakob@kirei.se>
2020-09-07 08:51:23 +0200

    fix formatting

--- src/cryptojwt/key_bundle.py
+++ src/cryptojwt/key_bundle.py
@@ -417,7 +417,9 @@ class KeyBundle:
 
         else:
             LOGGER.warning(
-                "HTTP status %d reading remote JWKS from %s", _http_resp.status_code, self.source,
+                "HTTP status %d reading remote JWKS from %s",
+                _http_resp.status_code,
+                self.source,
             )
             self.last_error = time.time()
             raise UpdateFailed(REMOTE_FAILED.format(self.source, _http_resp.status_code))
