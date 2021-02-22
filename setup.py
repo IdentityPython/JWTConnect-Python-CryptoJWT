64b1d837eb9b7353ee5e7f2f63ea6b9194d00008
Jakob Schlyter <jakob@kirei.se>
2021-02-22 15:42:18 +0100

    add 3.6

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
