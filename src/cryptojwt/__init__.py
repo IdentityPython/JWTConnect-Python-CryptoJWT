6e642be4a9502ade61a3cf5868093ac0bca4e18c
Jakob Schlyter <jakob@kirei.se>
2024-05-29 09:55:39 +0200

    use importlib.metadata

--- src/cryptojwt/__init__.py
+++ src/cryptojwt/__init__.py
@@ -2,7 +2,7 @@
 
 import logging
 
-import pkg_resources
+from importlib.metadata import version
 
 from cryptojwt.jwe.jwe import JWE
 from cryptojwt.jwk import JWK
@@ -24,7 +24,7 @@ try:
 except ImportError:
     pass
 
-__version__ = pkg_resources.get_distribution("cryptojwt").version
+__version__ = version("cryptojwt")
 
 logger = logging.getLogger(__name__)
 
