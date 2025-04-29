e500b36b9c3199699bb1cbb18a1fa9695eed45f7
Roland Hedberg <roland@catalogix.se>
2025-04-29 17:04:22 +0200

    Bli explicit about salt length when doing PSS padding.

---
+++ tests/test_21_pss.py
@@ -0,0 +1,29 @@
+import json
+
+import pytest
+
+from cryptojwt.jwk.jwk import key_from_jwk_dict
+from cryptojwt.jws.jws import JWS
+import test_vector
+
+
+@pytest.mark.parametrize(
+    "alg",
+    ["RS256", "RS384", "RS512", "PS256", "PS384", "PS512"]
+)
+def test_jws_rsa_signer_and_verifier(alg):
+    _jwk_dict = json.loads(test_vector.json_rsa_priv_key)
+    _key = key_from_jwk_dict(_jwk_dict)
+    _key.alg = alg
+    _key.add_kid()
+
+    json_header_rsa = json.loads(test_vector.test_header_rsa)
+    json_header_rsa["alg"] = alg
+
+    # Sign
+    jws = JWS(msg=test_vector.test_payload, **json_header_rsa)
+    signed_token = jws.sign_compact([_key])
+
+    # Verify
+    verifier = JWS(alg=[alg])
+    assert verifier.verify_compact(signed_token, [_key])
